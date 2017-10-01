a644b2892c17d3e423ab5b8e8bea02e2044192fa
Jannis R <mail@jannisr.de>
2017-10-01 17:29:03 +0200

    basic querying

--- index.js
+++ index.js
@@ -1,3 +1,66 @@
 'use strict'
 
-// todo
+const parallel = require('async/parallel')
+// const intersectionBy = require('lodash/intersectionBy')
+const minBy = require('lodash/minBy')
+
+const SEP = ':'
+// const BATCH_SIZE = 100
+
+const queryToken = (db, token) => (cb) => {
+	const entries = db.createReadStream({
+		gt: token + SEP + '!',
+		lt: token + SEP + '~',
+		reverse: true
+		// todo: limit
+	})
+	const results = {}
+	let done = false
+
+	entries.on('data', (entry) => {
+		const key = entry.key.split(SEP)
+		if (key.length !== 3) return
+
+		const docId = key[2]
+		const freq = parseFloat(entry.value)
+		if (!Number.isNaN(freq)) results[docId] = freq
+	})
+	entries.once('error', (err) => {
+		done = true
+		cb(err)
+	})
+	entries.once('end', () => {
+		if (!done) cb(null, results)
+	})
+}
+
+// todo: fuzzy completion, based on partially matched tokens
+const query = (db, tokens, cb) => {
+	const tasks = Object.create(null)
+	for (let token of tokens) tasks[token] = queryToken(db, token)
+
+	parallel(tasks, (err, sets) => {
+		if (err) return cb(err)
+
+		const tokenWithLeast = minBy(tokens, t => Object.keys(sets[t]).length)
+		for (let docId in sets[tokenWithLeast]) {
+			let inEvery = true, relevance = 0
+			for (let token of tokens) {
+				const set = sets[token]
+
+				if (docId in set) {
+					relevance += set[docId]
+				} else {
+					inEvery = false
+					break
+				}
+			}
+
+			if (inEvery) {
+				console.error(docId, relevance)
+			}
+		}
+	})
+}
+
+module.exports = query
